964e9a81c325d0442e3e582b1b2838b52e931a6b
idkNicks <yangdaeyeong0808@gmail.com>
2022-12-18 23:17:23 +0900

    🔸 Update Project

---
+++ src/main/java/com/github/nicks/region/Region.java
@@ -0,0 +1,12 @@
+package com.github.nicks.region;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Region extends JavaPlugin {
+
+
+    @Override
+    public void onEnable() {
+
+    }
+}
