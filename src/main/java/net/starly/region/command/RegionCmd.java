913f5f2be8b4aa350236b522ce5866bfc22306bb
yejunho10 <hyjcompany30@naver.com>
2022-12-26 16:58:31 +0900

    feat: `/region` command messages

--- src/main/java/net/starly/region/command/RegionCmd.java
+++ src/main/java/net/starly/region/command/RegionCmd.java
@@ -19,17 +19,23 @@ public class RegionCmd implements CommandExecutor {
                     if (args.length == 2) {
                         if (!RegionMapData.pos1Map.containsKey(player) || !RegionMapData.pos2Map.containsKey(player)) {
                             player.sendMessage("§c좌표를 선택해주세요.");
+
                             return true;
                         }
 
-                        if (!RegionMapData.regionMap.containsKey(args[1]))
+                        if (!RegionMapData.regionMap.containsKey(args[1])) {
                             RegionMapData.regionMap.put(args[1], new Region(RegionMapData.pos1Map.get(player), RegionMapData.pos2Map.get(player)));
+                            player.sendMessage("§a성공적으로 §f" + args[1] + " §a이름의 구역을 생성했습니다.");
+
+                            return true;
+                        }
                         else
-                            player.sendMessage("§c이미 존재하는 지역 이름입니다.");
+                            player.sendMessage("§c이미 존재하는 구역 이름입니다.");
 
                         return true;
                     } else {
                         player.sendMessage("§c사용법: /region 생성 <이름>");
+
                         return true;
                     }
 
@@ -39,11 +45,14 @@ public class RegionCmd implements CommandExecutor {
                     if (args.length == 2) {
                         if (RegionMapData.regionMap.containsKey(args[1])) {
                             RegionMapData.regionMap.remove(args[1]);
-                            player.sendMessage("§a성공적으로 지역을 제거했습니다.");
-                        } else
-                            player.sendMessage("§c존재하지 않는 지역 이름입니다.");
+                            player.sendMessage("§a성공적으로 구역을 제거했습니다.");
 
-                        return true;
+                            return true;
+                        } else {
+                            player.sendMessage("§c존재하지 않는 구역 이름입니다.");
+
+                            return true;
+                        }
                     } else {
                         player.sendMessage("§c사용법: /region 제거 <이름>");
                         return true;
@@ -58,10 +67,13 @@ public class RegionCmd implements CommandExecutor {
                                     .append(key)
                                     .append(", ");
                         }
-                        player.sendMessage("§a지역 목록: " + sb);
+
+                        player.sendMessage("§a구역 목록: " + sb);
+
                         return true;
                     } else {
                         player.sendMessage("§c사용법: /region 목록");
+
                         return true;
                     }
                 }
